07fe778e7034bbd4c372c1177ad763bbd59d162d
diego <diegoalonsonm@gmail.com>
2024-06-03 16:16:48 -0600

    firts theming

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker/widgets/expenses.dart';
 
+// initializing a variable with a k "makes it" global
+// this is a color scheme that will be used throughout the app with the color passed on the seedColor parameter
+var kColorScheme = ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 114, 21, 8));
+
 void main() {
   runApp (
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8)
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          )
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontSize: 22,
+            fontWeight: FontWeight.bold,
+            color: kColorScheme.secondaryContainer
+          ),
+        )
+      ),
       home: const Expenses(),
     )
   );
